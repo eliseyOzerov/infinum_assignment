c82c9a5a4b272b89b46d0e03efcddd6096a0058c
Elisey <eliseyozerov@outlook.com>
2022-02-06 17:07:44 +0100

    Add initialization logic
    2 hours (mainly deciding on the implementation)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
-import 'package:tv_shows/Controllers/UserController.dart';
-import 'package:tv_shows/Screens/LoginScreen.dart';
+import 'package:tv_shows/Screens/MainScreen.dart';
+import 'package:tv_shows/Screens/InitScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,45 +12,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<UserController>(create: (_) => UserController()),
-      ],
-      child: MaterialApp(
-        theme: ThemeData(
-          textTheme: GoogleFonts.karlaTextTheme(),
-          inputDecorationTheme: const InputDecorationTheme(
-            labelStyle: TextStyle(color: Color(0xFFA0A0A0), letterSpacing: 0),
-            focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color(0xFFDDDEDF))),
-            enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color(0xFFDDDEDF))),
-          ),
-          textButtonTheme: TextButtonThemeData(
-            style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.resolveWith((states) {
-                if (states.contains(MaterialState.disabled)) {
-                  return const Color(0xFFFFCCD5);
-                } else {
-                  return const Color(0xFFFF758C);
-                }
-              }),
-              foregroundColor: MaterialStateProperty.resolveWith((states) => Colors.white),
-              fixedSize: MaterialStateProperty.resolveWith((states) => const Size.fromHeight(48)),
-              shape: MaterialStateProperty.resolveWith(
-                (states) => const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(6)),
-                ),
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.karlaTextTheme(),
+        inputDecorationTheme: const InputDecorationTheme(
+          labelStyle: TextStyle(color: Color(0xFFA0A0A0), letterSpacing: 0),
+          focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color(0xFFDDDEDF))),
+          enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Color(0xFFDDDEDF))),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.disabled)) {
+                return const Color(0xFFFFCCD5);
+              } else {
+                return const Color(0xFFFF758C);
+              }
+            }),
+            foregroundColor: MaterialStateProperty.resolveWith((states) => Colors.white),
+            fixedSize: MaterialStateProperty.resolveWith((states) => const Size.fromHeight(48)),
+            shape: MaterialStateProperty.resolveWith(
+              (states) => const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(6)),
               ),
-              textStyle: MaterialStateProperty.resolveWith(
-                (states) => GoogleFonts.karla(
-                  fontSize: 16,
-                ),
+            ),
+            textStyle: MaterialStateProperty.resolveWith(
+              (states) => GoogleFonts.karla(
+                fontSize: 16,
               ),
             ),
           ),
-          primaryColor: const Color(0xFFFF758C),
         ),
-        home: const LoginScreen(),
+        primaryColor: const Color(0xFFFF758C),
       ),
+      home: const InitScreen(),
+      routes: {
+        "/main": (ctx) => const MainScreen(),
+      },
     );
   }
 }
