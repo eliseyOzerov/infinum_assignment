b67f0d6874b93a56ae001a2fa03e9c0ea3059f6b
Elisey <eliseyozerov@outlook.com>
2022-02-06 20:54:51 +0100

    Add the DI methods

--- lib/Utilities/DependencyInjector.dart
+++ lib/Utilities/DependencyInjector.dart
@@ -1,7 +1,12 @@
+import 'package:tv_shows/Controllers/ListOfShowsScreenController.dart';
 import 'package:tv_shows/Controllers/LoginScreenController.dart';
-import 'package:tv_shows/Managers/LocalStorage.dart';
+import 'package:tv_shows/Controllers/ShowDetailsScreenController.dart';
+import 'package:tv_shows/Managers/SecureLocalStorage.dart';
+import 'package:tv_shows/Models/ShowModel.dart';
+import 'package:tv_shows/Screens/ShowDetailsScreen.dart';
 import 'package:tv_shows/Screens/LoginScreen.dart';
-import 'package:tv_shows/Screens/MainScreen.dart';
+import 'package:tv_shows/Screens/ListOfShowsScreen.dart';
+import 'package:tv_shows/Services/ShowsApi.dart';
 import 'package:tv_shows/Services/UserApi.dart';
 
 // This is the first time I'm doing dependency injection at this level. This approach (one DependencyInjector class) seems good enough
@@ -14,13 +19,24 @@ class DependencyInjector {
   LoginScreen makeLoginScreen() {
     final controller = LoginScreenController(
       userApi: UserApi.shared,
-      localStorage: LocalStorage.shared,
+      secureLocalStorage: SecureLocalStorage.shared,
     );
     // we will set the router in the build method of LoginScreen, since we need the LoginScreen context
     return LoginScreen(controller: controller);
   }
 
-  MainScreen makeMainScreen() {
-    return const MainScreen();
+  ListOfShowsScreen makeListOfShowsScreen() {
+    final controller = ListOfShowsScreenController(
+      showsApi: ShowsApi.shared,
+      secureLocalStorage: SecureLocalStorage.shared,
+    );
+    return ListOfShowsScreen(controller: controller);
+  }
+
+  ShowDetailsScreen makeShowDetailsScreen(ShowModel show) {
+    final controller = ShowDetailsScreenController(
+      showsApi: ShowsApi.shared,
+    );
+    return ShowDetailsScreen(controller: controller, show: show);
   }
 }
