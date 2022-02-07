21bc47ba9c403c7f812bf94c1119d2fcdddf7526
Elisey <eliseyozerov@outlook.com>
2022-02-07 11:42:21 +0100

    Add onTap functionality to the EpisodeRow

--- lib/Widgets/EpisodeRow.dart
+++ lib/Widgets/EpisodeRow.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+import 'package:tv_shows/Controllers/ShowDetailsScreenController.dart';
 import 'package:tv_shows/Models/EpisodeModel.dart';
 import 'package:tv_shows/Utilities/Constants.dart';
 
@@ -10,30 +12,35 @@ class EpisodeRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: LARGE_PADDING, vertical: 18),
-      child: Row(
-        children: [
-          Text(
-            "S${episode.season} Ep${episode.episodeNumber}",
-            style: TextStyle(
-              fontSize: 16,
-              color: Theme.of(context).primaryColor,
-            ),
-          ),
-          const SizedBox(width: 12),
-          Expanded(
-            child: Text(
-              episode.title,
-              maxLines: 1,
-              style: const TextStyle(
+    final controller = context.read<ShowDetailsScreenController>();
+    return GestureDetector(
+      onTap: () => controller.navigateToEpisodeDetails(episode),
+      behavior: HitTestBehavior.opaque,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: LARGE_PADDING, vertical: 18),
+        child: Row(
+          children: [
+            Text(
+              "S${episode.season} Ep${episode.episodeNumber}",
+              style: TextStyle(
                 fontSize: 16,
-                color: Color(0xFF2E2E2E),
+                color: Theme.of(context).primaryColor,
               ),
             ),
-          ),
-          SvgPicture.asset("assets/icons/ic-navigation-chevron-right-medium.svg"),
-        ],
+            const SizedBox(width: 12),
+            Expanded(
+              child: Text(
+                episode.title,
+                maxLines: 1,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Color(0xFF2E2E2E),
+                ),
+              ),
+            ),
+            SvgPicture.asset("assets/icons/ic-navigation-chevron-right-medium.svg"),
+          ],
+        ),
       ),
     );
   }
