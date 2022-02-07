031be642ce5e027b21d37f108a097e0b1b6d0613
Elisey <eliseyozerov@outlook.com>
2022-02-07 11:02:37 +0100

    Add ShowDetailsScreen
    2 hours (Large chunk of that went into setting the color of the status bar correctly and figuring out the SliverAppBar)

---
+++ lib/Models/EpisodeModel.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+class EpisodeModel {
+  final String id;
+  final String title;
+  final String description;
+  final String imageUrl;
+  final String episodeNumber;
+  final String season;
+
+  EpisodeModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    required this.episodeNumber,
+    required this.season,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      '_id': id,
+      'title': title,
+      'description': description,
+      'imageUrl': imageUrl,
+      'episodeNumber': episodeNumber,
+      'season': season,
+    };
+  }
+
+  factory EpisodeModel.fromMap(Map<String, dynamic> map) {
+    return EpisodeModel(
+      id: map['_id'] ?? '',
+      title: map['title'] ?? '',
+      description: map['description'] ?? '',
+      imageUrl: map['imageUrl'] ?? '',
+      episodeNumber: map['episodeNumber'] ?? '',
+      season: map['season'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory EpisodeModel.fromJson(String source) => EpisodeModel.fromMap(json.decode(source));
+}
--- lib/Utilities/Constants.dart
+++ lib/Utilities/Constants.dart
@@ -1,3 +1,4 @@
 const double PADDING = 16;
+const double LARGE_PADDING = 24;
 
 const String BASE_URL = "https://api.infinum.academy";
